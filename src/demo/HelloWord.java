90ebb0f3283182c38a631996886478600162fec5
付明翰 <fuminghan@bonc.com.cn>
2016-10-13 12:00:59 +0800

    init

---
+++ src/demo/HelloWord.java
@@ -0,0 +1,14 @@
+package demo;
+/** 
+ * @author  fuminghan@bonc.com.cn: 
+ * @date 2016年10月13日 上午11:55:36 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("helloword!!!");
+	}
+}
